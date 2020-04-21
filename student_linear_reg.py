a36fa9c3e7d38ecacb7331c3026ba9e7d21a1207
Shaurya Choudhary <shauryachoudhary.2009@gmail.com>
2020-04-21 10:06:48 +0530

    Add files via upload

---
+++ student_linear_reg.py
@@ -0,0 +1,55 @@
+import pickle
+
+import matplotlib.pyplot as pyplot
+import numpy as np
+import pandas as pd
+from sklearn import model_selection
+from matplotlib import style
+
+data = pd.read_csv('student-mat.csv', sep=';')
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+predict = "G3"
+
+X = np.array(data.drop([predict], 1))
+Y = np.array(data[predict])
+x_train, x_test, y_train, y_test = model_selection.train_test_split(X, Y, test_size=0.1)
+
+# best = 0
+#
+# for _ in range(10000):
+#
+#     # test size represents the fraction of data preserved as test data-set
+#     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+#
+#     linear = linear_model.LinearRegression()
+#
+#     linear.fit(x_train, y_train)
+#     acc = linear.score(x_test, y_test)
+#     print(acc, end='\n\n')
+#
+#     if acc > best:
+#         best = acc
+#         with open("studentmodel.pickle", "wb") as f:
+#             pickle.dump(linear, f)
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+acc = linear.score(x_test, y_test)
+print(acc, end='\n\n')
+
+print("Coefficients : \n", linear.coef_)
+print("Intercept : \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
+
+style.use("ggplot")
+p = "absences"
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
