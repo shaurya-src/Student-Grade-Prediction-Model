5feed415fef6efd8c7f32bc6e3db1ff74c48100d
Shaurya Choudhary <shauryachoudhary.2009@gmail.com>
2020-11-11 17:57:39 +0530

    Update student_linear_reg.py

--- student_linear_reg.py
+++ student_linear_reg.py
@@ -6,34 +6,32 @@ import pandas as pd
 from sklearn import model_selection
 from matplotlib import style
 
+# reading dataset
 data = pd.read_csv('student-mat.csv', sep=';')
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
 predict = "G3"
 
+# spilitting the dataset for training and testing
 X = np.array(data.drop([predict], 1))
 Y = np.array(data[predict])
 x_train, x_test, y_train, y_test = model_selection.train_test_split(X, Y, test_size=0.1)
 
-# best = 0
-#
-# for _ in range(10000):
-#
-#     # test size represents the fraction of data preserved as test data-set
-#     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
-#
-#     linear = linear_model.LinearRegression()
-#
-#     linear.fit(x_train, y_train)
-#     acc = linear.score(x_test, y_test)
-#     print(acc, end='\n\n')
-#
-#     if acc > best:
-#         best = acc
-#         with open("studentmodel.pickle", "wb") as f:
-#             pickle.dump(linear, f)
+ best = 0
 
-pickle_in = open("studentmodel.pickle", "rb")
+for _ in range(10000):
+    # test size represents the fraction of data preserved as test data-set
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+    linear = linear_model.LinearRegression()
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print(acc, end='\n\n')
+    if acc > best:
+        best = acc
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
+
+            pickle_in = open("studentmodel.pickle", "rb")
 linear = pickle.load(pickle_in)
 
 acc = linear.score(x_test, y_test)
